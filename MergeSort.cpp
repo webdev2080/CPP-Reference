92da394560bf00971e726e3d6333235a2430ea51
WebDev2080 <52177935+webdev2080@users.noreply.github.com>
2020-09-23 09:14:31 -0700

    Create MergeSort.cpp

---
+++ MergeSort.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+void Merge(int numbers[], int i, int j, int k) {
+   int mergedSize = k - i + 1;                // Size of merged partition
+   int mergePos = 0;                          // Position to insert merged number
+   int leftPos = 0;                           // Position of elements in left partition
+   int rightPos = 0;                          // Position of elements in right partition
+   int* mergedNumbers = new int[mergedSize];  // Dynamically allocates temporary array
+                                              // for merged numbers
+   
+   leftPos = i;                               // Initialize left partition position
+   rightPos = j + 1;                          // Initialize right partition position
+   
+   // Add smallest element from left or right partition to merged numbers
+   while (leftPos <= j && rightPos <= k) {
+      if (numbers[leftPos] < numbers[rightPos]) {
+         mergedNumbers[mergePos] = numbers[leftPos];
+         ++leftPos;
+      }
+      else {
+         mergedNumbers[mergePos] = numbers[rightPos];
+         ++rightPos;
+         
+      }
+      ++mergePos;
+   }
+   
+   // If left partition is not empty, add remaining elements to merged numbers
+   while (leftPos <= j) {
+      mergedNumbers[mergePos] = numbers[leftPos];
+      ++leftPos;
+      ++mergePos;
+   }
+   
+   // If right partition is not empty, add remaining elements to merged numbers
+   while (rightPos <= k) {
+      mergedNumbers[mergePos] = numbers[rightPos];
+      ++rightPos;
+      ++mergePos;
+   }
+   
+   // Copy merge number back to numbers
+   for (mergePos = 0; mergePos < mergedSize; ++mergePos) {
+      numbers[i + mergePos] = mergedNumbers[mergePos];
+   }
+}
+
+void MergeSort(int numbers[], int i, int k) {
+   int j = 0;
+   
+   if (i < k) {
+      j = (i + k) / 2;  // Find the midpoint in the partition
+      
+      // Recursively sort left and right partitions
+      MergeSort(numbers, i, j);
+      MergeSort(numbers, j + 1, k);
+      
+      // Merge left and right partition in sorted order
+      Merge(numbers, i, j, k);
+   }
+}
+
+int main() {
+   int numbers[] = { 10, 2, 78, 4, 45, 32, 7, 11 };
+   const int NUMBERS_SIZE = 8;
+   int i = 0;
+   
+   cout << "UNSORTED: ";
+   for(i = 0; i < NUMBERS_SIZE; ++i) {
+      cout << numbers[i] << " ";
+   }
+   cout << endl;
+   
+   MergeSort(numbers, 0, NUMBERS_SIZE - 1);
+   
+   cout << "SORTED: ";
+   for(i = 0; i < NUMBERS_SIZE; ++i) {
+      cout << numbers[i] << " ";
+   }
+   cout << endl;
+   
+   return 0;
+}
